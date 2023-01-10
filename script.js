537ba2c481d76fbb4a0ee7c59c21f9d0ac7eb7a3
Shivam7264 <shuklashivam20001@gmail.com>
2023-01-10 16:56:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,21 +4,21 @@ let btn1El=document.getElementById("btn1")
 let lengthEl=document.getElementById("length")
 let volumeEl=document.getElementById("volume")
 let weightEl=document.getElementById("weight")
-let arr=[]
+let arr
 btnEl.addEventListener("click",function(){
-    arr.push(boxEl.value)
+    arr=boxEl.value
    
     print(arr)
-    arr.pop()
+   
 })
 
 function print(arr){
-    let x=`${arr[0]} meters = ${(arr[0]*3.28084).toFixed(2)} feet | ${arr[0]} feet = ${(arr[0]*0.3048).toFixed(2)} meter `
+    let x=`${arr} meters = ${(arr*3.28084).toFixed(2)} feet | ${arr} feet = ${(arr*0.3048).toFixed(2)} meter `
 lengthEl.innerHTML=x
-let y=`${arr[0]} Litres = ${(arr[0]*0.264172).toFixed(2)} gallons | ${arr[0]} gallon = ${(arr[0]*3.78541).toFixed(2)} liters `
+let y=`${arr} Litres = ${(arr*0.264172).toFixed(2)} gallons | ${arr} gallon = ${(arr*3.78541).toFixed(2)} liters `
 volumeEl.innerHTML=y
 
-let z=`${arr[0]} kilograms = ${(arr[0]*2.20462).toFixed(2)} pounds | ${arr[0]} pounds = ${(arr[0]*0.453592).toFixed(2)} kilograms `
+let z=`${arr} kilograms = ${(arr*2.20462).toFixed(2)} pounds | ${arr} pounds = ${(arr*0.453592).toFixed(2)} kilograms `
 weightEl.innerHTML=z
 }
 
