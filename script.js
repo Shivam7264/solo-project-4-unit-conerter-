b6955d90656d47e7010e3a3ccdc5b70939076db0
Shivam7264 <shuklashivam20001@gmail.com>
2023-01-10 16:00:04 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let boxEl=document.getElementById("box")
+let btnEl=document.getElementById("btn")
+let btn1El=document.getElementById("btn1")
+let lengthEl=document.getElementById("length")
+let volumeEl=document.getElementById("volume")
+let weightEl=document.getElementById("weight")
+let arr=[]
+btnEl.addEventListener("click",function(){
+    arr.push(boxEl.value)
+    localStorage.setItem("arr",JSON.stringify(arr))
+    print(arr)
+})
+
+function print(arr){
+    const x=`${arr[0]} meters = ${(arr[0]*3.28084).toFixed(2)} feet | ${arr[0]} feet = ${(arr[0]*0.3048).toFixed(2)} meter `
+lengthEl.innerHTML=x
+const y=`${arr[0]} Litres = ${(arr[0]*0.264172).toFixed(2)} gallons | ${arr[0]} gallon = ${(arr[0]*3.78541).toFixed(2)} liters `
+volumeEl.innerHTML=y
+
+const z=`${arr[0]} kilograms = ${(arr[0]*2.20462).toFixed(2)} pounds | ${arr[0]} pounds = ${(arr[0]*0.453592).toFixed(2)} kilograms `
+weightEl.innerHTML=z
+}
+
+btn1El.addEventListener("click",function(){
+    lengthEl.innerHTML=""
+    volumeEl.innerHTML=""
+    weightEl.innerHTML=""
+    boxEl.value=""
+})
