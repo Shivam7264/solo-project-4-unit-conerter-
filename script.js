e8b8edca0b91f1c8132bbbd6fc3880de83a97cfa
Shivam7264 <shuklashivam20001@gmail.com>
2023-01-10 16:43:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,18 +7,18 @@ let weightEl=document.getElementById("weight")
 let arr=[]
 btnEl.addEventListener("click",function(){
     arr.push(boxEl.value)
-    localStorage.setItem("arr",JSON.stringify(arr))
+   
     print(arr)
     arr.pop()
 })
 
 function print(arr){
-    const x=`${arr[0]} meters = ${(arr[0]*3.28084).toFixed(2)} feet | ${arr[0]} feet = ${(arr[0]*0.3048).toFixed(2)} meter `
+    let x=`${arr[0]} meters = ${(arr[0]*3.28084).toFixed(2)} feet | ${arr[0]} feet = ${(arr[0]*0.3048).toFixed(2)} meter `
 lengthEl.innerHTML=x
-const y=`${arr[0]} Litres = ${(arr[0]*0.264172).toFixed(2)} gallons | ${arr[0]} gallon = ${(arr[0]*3.78541).toFixed(2)} liters `
+let y=`${arr[0]} Litres = ${(arr[0]*0.264172).toFixed(2)} gallons | ${arr[0]} gallon = ${(arr[0]*3.78541).toFixed(2)} liters `
 volumeEl.innerHTML=y
 
-const z=`${arr[0]} kilograms = ${(arr[0]*2.20462).toFixed(2)} pounds | ${arr[0]} pounds = ${(arr[0]*0.453592).toFixed(2)} kilograms `
+let z=`${arr[0]} kilograms = ${(arr[0]*2.20462).toFixed(2)} pounds | ${arr[0]} pounds = ${(arr[0]*0.453592).toFixed(2)} kilograms `
 weightEl.innerHTML=z
 }
 
